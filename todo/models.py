6081a6ac4165e354d37644c386a358b4372c53b2
Folafolu <ofolafolu@gmail.com>
2023-02-24 03:43:53 +0100

    created todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    content = models.CharField(max_length=100)
+    created_on = models.DateField(auto_now_add=True)   
+    updated_on = models.DateField(auto_now=True)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from . models import Todo
 # Create your views here.
+
+
+def home(request):
+    todo_list = Todo.objects.all()
+    context = {'todo_list': todo_list}
+    return render(request, 'index.html', context=context)
+
+
+def create_todo(request):
+    return HttpResponse("Create")
