8d83a75673868ca87796606d7bcab593be838712
Folafolu <ofolafolu@gmail.com>
2023-02-26 13:40:59 +0100

    create and delete todos

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('delete/<int:pk>/', views.delete_todo, name='delete')
 ]
+
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 from . models import Todo
+from . forms import TodoForm
 # Create your views here.
 
 
 def home(request):
     todo_list = Todo.objects.all()
-    context = {'todo_list': todo_list}
-    return render(request, 'index.html', context=context)
+    if request.method == 'GET':
+        form = TodoForm()
+    elif request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/todo')
+    return render(request, 'index.html', {'todo_list':todo_list, 'form':form})
 
 
-def create_todo(request):
-    return HttpResponse("Create")
+
+def delete_todo(request, pk):
+    item = get_object_or_404(Todo, pk=pk)
+    item.delete()
+    return redirect('/todo')
+
